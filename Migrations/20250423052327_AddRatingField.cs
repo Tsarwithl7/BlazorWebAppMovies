4add58fe40d657e0ba34bc0b4c27f978617bd7a6
huangzhidong <huangzhidong@liany.com.cn>
2025-04-23 15:22:15 +0800

    添加项目文件。

---
+++ Migrations/20250423052327_AddRatingField.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebAppMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(35)",
+                maxLength: 35,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "R");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(35)",
+                oldMaxLength: 35);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWebAppMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "电影名称是必须的")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "电影分级是必须的")]
+        [RegularExpression(@"^(G|PG|PG-13|R|NC-17)$")]
+        public string? Rating { get; set; }
+
+
+        public DateOnly ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "电影类型是必须的")]
+        [StringLength(35, ErrorMessage = "电影类型不能超过35个字符")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$",ErrorMessage = "Genre must be in capital letters with no special characters")]
+        public string? Genre { get; set; }
+
+        [Range(0,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+
+    }
+}
